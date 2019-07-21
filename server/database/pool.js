c8f842de63e58dbeac993a66caf89e0a71a10eee
Oseikweme Benjamin Isidahomen <oseikwemeisidahomen@gmail.com>
2019-07-21 16:48:00 +0100

    Added make runnable

--- server/database/pool.js
+++ server/database/pool.js
@@ -3,13 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const environments = {
-  development: process.env.DATABASE_URL,
-  test: process.env.TEST_DATABASE_URL
-};
-
-const nodeEnv = process.env.NODE_ENV || 'development';
-const connectionString = environments[nodeEnv];
+const connectionString = process.env.DATABASE_URL;
 
 const connect = {
   connectionString
