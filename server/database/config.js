f27b269eb0f5dddae5f68e85435d853b664ed8cb
Oseikweme Benjamin Isidahomen <oseikwemeisidahomen@gmail.com>
2019-07-21 21:52:43 +0100

    Adding team

--- server/database/config.js
+++ server/database/config.js
@@ -45,6 +45,17 @@ const createTables = async () => {
     await pool.query(createMembersQuery, ['Rita', 1]);
     await pool.query(createMembersQuery, ['Ovie', 1]);
     await pool.query(createMembersQuery, ['Bela', 1]);
+    await pool.query(createMembersQuery, ['Nero', 1]);
+    await pool.query(createMembersQuery, ['Ben', 1]);
+    await pool.query(createMembersQuery, ['Maro', 1]);
+    await pool.query(createTeamQuery, ['Mazus']);
+    await pool.query(createMembersQuery, ['Odum', 1]);
+    await pool.query(createMembersQuery, ['Pelumi', 1]);
+    await pool.query(createMembersQuery, ['Tolu', 1]);
+    await pool.query(createMembersQuery, ['Emeka', 1]);
+    await pool.query(createMembersQuery, ['John', 1]);
+    await pool.query(createMembersQuery, ['Mark', 1]);
+    await pool.query(createMembersQuery, ['Peace', 1]);
   } catch (e) {
     throw e;
   }
